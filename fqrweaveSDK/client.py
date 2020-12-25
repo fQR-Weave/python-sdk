f8b978221116179b531a15b8dab23c0b515dcc11
wojak-frog <37157916+wojak-frog@users.noreply.github.com>
2020-12-25 23:47:32 +0000

    Update client.py
    - added fQR generation from CSV datasets
    - tx_ID validity checker

--- fqrweaveSDK/client.py
+++ fqrweaveSDK/client.py
@@ -1,8 +1,10 @@
 import arweave
 import requests
-import os, sys
+import os
+import sys
 import qrcode
 from bs4 import BeautifulSoup as BS
+import pandas as pd
 
 
 KEYFILE_PATH = '/path/to/keyfile.json'
@@ -91,27 +93,44 @@ class Tools(object):
 
         #decode from bytestr to ascii, then split it
         #into a list
-        return len((str(last_tx.data, 'utf8')).split(' '))
-                
-        # __version="0.0.2"__
+        return (str(last_tx.data, 'utf8')).split(' ')
+
+    def isverified(self, tx_id):
+        """
+
+        :param tx_id:
+        :return:
+        """
+        n_list = Tools().get_n_generators()
+        re = requests.get(f'https://arweave.net/tx/{tx_id}/owner').text
+        if re in n_list:
+            return ('found, gennerated from a verified generator')
+        else:
+            return ('invalid tx id / not verified')
+
+
+class Generator(object):
+    def __init__(self):
+        self.wallet = connect(KEYFILE_PATH)
+
     def batch_generator(self, instance, data_list, dir_name):
         """
-        this function scrap generator's instance to extract 
+        this function scrap generator's instance to extract
         <label>.value text , which represent fQR metadata.
-        
+
         Then the second parameter contains an array of
-        value to be passed to each label. 
-        
+        value to be passed to each label.
+
         After that a data tx is submitted to the blockweave
         with a pre_defined HTML template: _html_1 & _html_2
         and a tag('Content Type', 'text/html') to make the browser
         read the data as html
-        
+
         transaction.id is encoded to a QR code and saved as .png
         file in dir_name
-       
+
         :param instance: custom generator instance url
-        :param data_list: labels entries 
+        :param data_list: labels entries
         :param dir_name: path to directory to save the QRs.png
         :return:QR.png transaction's ID
         """
@@ -168,7 +187,7 @@ class Tools(object):
         transaction.send()
 
         qr = qrcode.QRCode(
-            version=1,
+            version=3,
             error_correction=qrcode.constants.ERROR_CORRECT_H,
             box_size=10,
             border=4,
@@ -180,3 +199,82 @@ class Tools(object):
         img.save(f'{path}\\{transaction.id}.png')
 
         return transaction.id
+
+    def csv_generator(self, file_path, instance, dir_path):  # path, generator instance
+        """
+
+        :param file_path:
+        :param instance:
+        :return:
+        """
+        # connect to generator instance
+        if instance.startswith('https://'):
+            url = instance
+        else:
+            url = f"https://arweave.net/{instance}"
+
+        ##############################################
+
+        # get generator entries
+        re = requests.get(url)
+        html = BS(re.text, features="lxml")
+        labels = html.find_all('label')
+
+        product_metadata = []  # store all generator instance metadata
+
+        _html_1 = '<html>' + '<head></head>' + \
+                  '<body> <center><h1>verified fQR</h1> </center>' + \
+                  '< br > < br > < center > < div > < table > < tr > < th > Data < / th > < th > info < / th > < / tr >'
+
+        _html_2 = '</table></div></body></html>'
+
+        for element in labels:
+            clean_1 = str(element).replace('<label for="product">', '')
+            clean_2 = str(clean_1).replace('</label>', '')
+            clean_3 = clean_2.split(':')
+            clean_4 = clean_3[0]
+            product_metadata.append(clean_4)
+
+        ################################################
+
+        df = pd.read_csv(file_path)
+        header = list(df.head(0).columns)
+        if header != product_metadata:
+            return 'unmatched entries'
+
+        rows = []
+
+        # add each a row as a list `rows` list
+        for i, j in df.iterrows():
+            rows.append(list(j))
+
+        # create array (_html_body) of arrays (metadata: data ; html table elements)
+        _html_body = []
+        for product in rows:
+            components = []  # stores each product's html table element in a array and append it to _html_body
+            for i in range(len(header)):
+                div = f'<tr><td>{header[i]}</td><td>{product[i]}</td></tr>'
+                components.append(div)
+            _html_body.append(components)
+
+        for html_el in _html_body:
+            fqr_html = _html_1 + ''.join(html_el) + _html_2
+            fqr = fqr_html.replace(' ', '')
+
+            transaction = arweave.Transaction(self.wallet, data=fqr)
+            transaction.add_tag('Content-Type', 'text/html')
+            transaction.sign()
+            transaction.send()
+
+            qr = qrcode.QRCode(
+                version=3,
+                error_correction=qrcode.constants.ERROR_CORRECT_H,
+                box_size=10,
+                border=4,
+            )
+            qr.add_data(transaction.id)
+            qr.make(fit=True)
+            path = os.path.join(sys.path[0], dir_path)
+            img = qr.make_image(fill_color="black", back_color="white")
+            img.save(f'{path}\\{transaction.id}.png')
+            return transaction.id
